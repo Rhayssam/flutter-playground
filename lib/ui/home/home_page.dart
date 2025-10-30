6bb8a891fce3de7397065adfb40b1f03bc8773ff
rhayssam <rhayssam@crmsolucoes.com.br>
2025-10-30 14:49:28 -0300

    feat: adicionando o CustomButton na HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
+import 'package:flutterplayground/ui/core/widgets/custom_button/custom_button.dart';
 import 'package:flutterplayground/ui/home/widgets/home_drawer.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
@@ -42,54 +43,9 @@ class HomePage extends GetView<HomeController> {
                 textAlign: TextAlign.center,
                 style: Theme.of(context).textTheme.displayMedium,
               ),
-              CustomizedButton(),
+              CustomButton(),
               const SizedBox(
                 height: 10,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CustomizedButton extends StatelessWidget {
-  const CustomizedButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondaryContainer,
-      borderRadius: BorderRadius.circular(10),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(10),
-        onTap: () => log('message'),
-        child: Ink(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Theme.of(context).colorScheme.outline,
-              width: 2,
-            ),
-          ),
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 5,
-            children: [
-              Text(
-                'Get Started',
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-              Text(
-                'Descrição',
-                style: Theme.of(context).textTheme.titleMedium,
               ),
             ],
           ),
