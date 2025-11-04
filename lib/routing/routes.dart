bf97cffe26350f352596547d52818af48702aa1d
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-04 17:46:19 -0300

    feat: adicionando a rota material no Routes

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,6 +1,7 @@
 final class Routes {
   static const String splash = '/splash';
   static const String home = '/home';
+  static const String material = '/material';
 
   static const String notFound =
       '/'; //! O Getx sempre redireciona para a rota raiz se a rota não for encontrada
