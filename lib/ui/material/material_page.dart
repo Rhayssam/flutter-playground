0ac8a366647dda8e8e0ae56f4bb78cdd5206e6ae
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-05 08:56:58 -0300

    feat: adicionando a CustomAppBar na MaterialPage

--- lib/ui/material/material_page.dart
+++ lib/ui/material/material_page.dart
@@ -1,16 +1,22 @@
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import './material_controller.dart';
 
 class MaterialPage extends GetView<MaterialController> {
-    
-    const MaterialPage({Key? key}) : super(key: key);
+  const MaterialPage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('MaterialPage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar.primaryContainer(
+        title: Text('Material Components'),
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Container(),
+    );
+  }
 }
