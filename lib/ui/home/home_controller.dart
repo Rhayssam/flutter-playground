967b76b34fc18f36d47ea4647656636cc84cce05
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-05 08:47:00 -0300

    feat: adicionando o método de navegação para a Material na HomeController

--- lib/ui/home/home_controller.dart
+++ lib/ui/home/home_controller.dart
@@ -1,3 +1,11 @@
+import 'package:flutterplayground/routing/routes.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  // Método para navegar para a página de Material Components
+  Future<void> goToMaterialPage() async {
+    await Get.toNamed(
+      Routes.material,
+    );
+  }
+}
