9c0aad3d484a98c05fe1668ce95a76502e72060f
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 16:18:41 -0300

    feat: adicionando o LinearProgressIndicatorPage

---
+++ lib/ui/material/pages/communication/linear_progress_indicator_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/app_bar_back_button.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
+
+class LinearProgressIndicatorPage extends StatelessWidget {
+  const LinearProgressIndicatorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar.primaryContainer(
+        title: Text('LinearProgressIndicator'),
+        leading: AppBarBackButton(),
+      ),
+      body: Container(),
+    );
+  }
+}
