05708bb0616d7e1699b5357c582cc4dad9c5d46b
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 14:36:48 -0300

    feat: adicionando a HomeController para a pasta de viewmodel

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutterplayground/ui/core/widgets/custom_button/custom_button.da
 import 'package:flutterplayground/ui/home/widgets/home_drawer.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import './home_controller.dart';
+import 'viewmodel/home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
