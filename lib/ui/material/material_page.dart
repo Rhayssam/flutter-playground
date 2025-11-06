f124f1bac4c3d1086682394efb4f08b39a5a39b8
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 14:18:04 -0300

    feat: extraindo widgets da MaterialPage

--- lib/ui/material/material_page.dart
+++ lib/ui/material/material_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutterplayground/ui/core/widgets/app_bar/app_bar_back_button.dart';
 import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
-import 'package:flutterplayground/ui/core/widgets/custom_button/custom_button.dart';
+import 'package:flutterplayground/ui/material/widgets/section/section.dart';
+import 'package:flutterplayground/ui/material/widgets/section/section_actions.dart';
+import 'package:flutterplayground/ui/material/widgets/section/section_comunication.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import './material_controller.dart';
@@ -12,10 +15,7 @@ class MaterialPage extends GetView<MaterialController> {
     return Scaffold(
       appBar: CustomAppBar.primaryContainer(
         title: Text('Material Components'),
-        leading: IconButton(
-          onPressed: () => Get.back(),
-          icon: Icon(Icons.arrow_back_ios),
-        ),
+        leading: AppBarBackButton(),
       ),
       body: SafeArea(
         child: Padding(
@@ -29,73 +29,12 @@ class MaterialPage extends GetView<MaterialController> {
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.displayMedium,
                 ),
-                Divider(
-                  color: Theme.of(context).colorScheme.outline,
-                ),
+                Divider(color: Theme.of(context).colorScheme.outline),
                 Text(
                   'O Flutter oferece uma variedade de widgets visuais, comportamentais e com recursos de movimento que implementam a especificação de design do Material 3. O Material 3 é a linguagem de design padrão do Flutter, permitindo que você crie e desenvolva aplicativos bonitos e fáceis de usar, que podem ser adaptados a qualquer plataforma.',
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.tertiaryContainer.withAlpha(60),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.outline.withAlpha(20),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      spacing: 10,
-                      children: [
-                        ExpansionTile(
-                          initiallyExpanded: true,
-                          title: Text(
-                            'Ações',
-                            style: Theme.of(context).textTheme.displaySmall,
-                          ),
-                          children: [
-                            Column(
-                              spacing: 15,
-                              children: [
-                                CustomButton(
-                                  title: 'Botões comuns',
-                                  content:
-                                      'Botões Clicáveis que iniciam uma ação, como enviar um e-mail, compartilhar um documento ou curtir um comentário.',
-                                  onTap: () {},
-                                ),
-                                CustomButton(
-                                  title: 'Botão de ação flutuante',
-                                  content:
-                                      'Bloco clicável contendo um ícone que mantém uma ação importante sempre ao alcance.',
-                                  onTap: () {},
-                                ),
-                                CustomButton(
-                                  title: 'Botão de ação flutuante estendido',
-                                  content:
-                                      'Bloco clicável que aciona uma ação. Esses blocos maiores podem exibir um rótulo de texto e oferecem uma área de interação maior.',
-                                  onTap: () {},
-                                ),
-                                CustomButton(
-                                  title: 'Botão de ícone',
-                                  content:
-                                      'Ícones clicáveis para incentivar os usuários do aplicativo a realizar ações complementares.',
-                                  onTap: () {},
-                                ),
-                                CustomButton(
-                                  title: 'Botão segmentado',
-                                  content:
-                                      'Blocos clicáveis, selecionáveis individualmente ou em conjunto, para ajudar os usuários a escolher opções, alterar entre visualizações ou classificar elementos.',
-                                  onTap: () {},
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                Section(sectionName: 'Ações', content: SectionActions()),
+                Section(sectionName: 'Comunication', content: SectionComunication()),
               ],
             ),
           ),
