9b21f6a054db89a406f55c2933d4563fd5937ec2
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 11:47:26 -0300

    feat: adicionando o ExpansionTile para as ações na MaterialPage

--- lib/ui/material/material_page.dart
+++ lib/ui/material/material_page.dart
@@ -29,15 +29,15 @@ class MaterialPage extends GetView<MaterialController> {
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.displayMedium,
                 ),
-                Text(
-                  'O Flutter oferece uma variedade de widgets visuais, comportamentais e com recursos de movimento que implementam a especificação de design do Material 3. O Material 3 é a linguagem de design padrão do Flutter, permitindo que você crie e desenvolva aplicativos bonitos e fáceis de usar, que podem ser adaptados a qualquer plataforma.',
-                ),
                 Divider(
                   color: Theme.of(context).colorScheme.outline,
                 ),
+                Text(
+                  'O Flutter oferece uma variedade de widgets visuais, comportamentais e com recursos de movimento que implementam a especificação de design do Material 3. O Material 3 é a linguagem de design padrão do Flutter, permitindo que você crie e desenvolva aplicativos bonitos e fáceis de usar, que podem ser adaptados a qualquer plataforma.',
+                ),
                 Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.tertiaryContainer.withAlpha(30),
+                    color: Theme.of(context).colorScheme.tertiaryContainer.withAlpha(60),
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                     border: Border.all(
                       color: Theme.of(context).colorScheme.outline.withAlpha(20),
@@ -48,43 +48,49 @@ class MaterialPage extends GetView<MaterialController> {
                     child: Column(
                       spacing: 10,
                       children: [
-                        Text(
-                          'Ações',
-                          textAlign: TextAlign.start,
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
-                        Divider(
-                          color: Theme.of(context).colorScheme.outline.withAlpha(50),
-                        ),
-                        CustomButton(
-                          title: 'Botões comuns',
-                          content:
-                              'Botões Clicáveis que iniciam uma ação, como enviar um e-mail, compartilhar um documento ou curtir um comentário.',
-                          onTap: () {},
-                        ),
-                        CustomButton(
-                          title: 'Botão de ação flutuante',
-                          content:
-                              'Bloco clicável contendo um ícone que mantém uma ação importante sempre ao alcance.',
-                          onTap: () {},
-                        ),
-                        CustomButton(
-                          title: 'Botão de ação flutuante estendido',
-                          content:
-                              'Bloco clicável que aciona uma ação. Esses blocos maiores podem exibir um rótulo de texto e oferecem uma área de interação maior.',
-                          onTap: () {},
-                        ),
-                        CustomButton(
-                          title: 'Botão de ícone',
-                          content:
-                              'Ícones clicáveis para incentivar os usuários do aplicativo a realizar ações complementares.',
-                          onTap: () {},
-                        ),
-                        CustomButton(
-                          title: 'Botão segmentado',
-                          content:
-                              'Blocos clicáveis, selecionáveis individualmente ou em conjunto, para ajudar os usuários a escolher opções, alterar entre visualizações ou classificar elementos.',
-                          onTap: () {},
+                        ExpansionTile(
+                          initiallyExpanded: true,
+                          title: Text(
+                            'Ações',
+                            style: Theme.of(context).textTheme.displaySmall,
+                          ),
+                          children: [
+                            Column(
+                              spacing: 15,
+                              children: [
+                                CustomButton(
+                                  title: 'Botões comuns',
+                                  content:
+                                      'Botões Clicáveis que iniciam uma ação, como enviar um e-mail, compartilhar um documento ou curtir um comentário.',
+                                  onTap: () {},
+                                ),
+                                CustomButton(
+                                  title: 'Botão de ação flutuante',
+                                  content:
+                                      'Bloco clicável contendo um ícone que mantém uma ação importante sempre ao alcance.',
+                                  onTap: () {},
+                                ),
+                                CustomButton(
+                                  title: 'Botão de ação flutuante estendido',
+                                  content:
+                                      'Bloco clicável que aciona uma ação. Esses blocos maiores podem exibir um rótulo de texto e oferecem uma área de interação maior.',
+                                  onTap: () {},
+                                ),
+                                CustomButton(
+                                  title: 'Botão de ícone',
+                                  content:
+                                      'Ícones clicáveis para incentivar os usuários do aplicativo a realizar ações complementares.',
+                                  onTap: () {},
+                                ),
+                                CustomButton(
+                                  title: 'Botão segmentado',
+                                  content:
+                                      'Blocos clicáveis, selecionáveis individualmente ou em conjunto, para ajudar os usuários a escolher opções, alterar entre visualizações ou classificar elementos.',
+                                  onTap: () {},
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ],
                     ),
