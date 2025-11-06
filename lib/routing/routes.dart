662f5b77c853ee7b854a6deb5174756ee5512761
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 15:28:12 -0300

    feat: adicionando novas rotas referente ao setor de Actions

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -2,6 +2,13 @@ final class Routes {
   static const String splash = '/splash';
   static const String home = '/home';
   static const String material = '/material';
+  // 1 - Rotas do Material
+  // 1.1 - Actions
+  static const String commonButtons = '/material/actions/common_buttons';
+  static const String fab = '/material/actions/fab';
+  static const String extendedFab = '/material/actions/extendedFab';
+  static const String iconButton = '/material/actions/iconButton';
+  static const String segmentedButton = '/material/actions/segmentedButton';
 
   static const String notFound =
       '/'; //! O Getx sempre redireciona para a rota raiz se a rota não for encontrada
