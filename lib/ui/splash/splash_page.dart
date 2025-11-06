6d227cc1e512d2f7b19674e174212505bc518e85
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 14:37:31 -0300

    feat: adicionando a SplashController para a pasta de viewmodel

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutterplayground/ui/splash/splash_controller.dart';
+import 'package:flutterplayground/ui/splash/viewmodel/splash_controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
