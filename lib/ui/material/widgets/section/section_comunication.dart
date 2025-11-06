6fceebccc7449a6b074e6ed4b200135df735c66f
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 16:22:48 -0300

    feat: adicionando os métodos na SectionCommunication

--- lib/ui/material/widgets/section/section_comunication.dart
+++ lib/ui/material/widgets/section/section_comunication.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutterplayground/ui/core/widgets/custom_button/custom_button.dart';
+import 'package:flutterplayground/ui/material/viewmodel/material_controller.dart';
+import 'package:get/get.dart';
 
 class SectionComunication extends StatelessWidget {
   const SectionComunication({
     super.key,
   });
 
+  MaterialController get _controller => Get.find<MaterialController>();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,19 +18,19 @@ class SectionComunication extends StatelessWidget {
           title: 'Badge',
           content:
               'Bloco semelhante a um ícone que transmite conteúdo dinâmico, como contagens ou status. Pode incluir rótulos ou números.',
-          onTap: () {},
+          onTap: _controller.goToBadgePage,
         ),
         CustomButton(
           title: 'LinearProgressIndicator',
           content:
               'Linha vertical que muda de cor à medida que um processo em andamento (como um carregamento ou envio de um formulário) é concluído.',
-          onTap: () {},
+          onTap: _controller.goToLinearProgressIndicatorPage,
         ),
         CustomButton(
           title: 'SnackBar',
           content:
               'Mensagens breves sobre os processos do aplicativo que são exibidas na parte inferior da tela.',
-          onTap: () {},
+          onTap: _controller.goToSnackbarPage,
         ),
       ],
     );
