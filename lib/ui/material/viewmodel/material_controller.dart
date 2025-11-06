7ca54f0195644466c3162d2851a7fc3dead8298c
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-06 15:30:24 -0300

    feat: criando os métodos de acesso para as páginas de Action na MaterialController

--- lib/ui/material/viewmodel/material_controller.dart
+++ lib/ui/material/viewmodel/material_controller.dart
@@ -1,3 +1,34 @@
+import 'package:flutterplayground/routing/routes.dart';
 import 'package:get/get.dart';
 
-class MaterialController extends GetxController {}
+class MaterialController extends GetxController {
+  Future<void> goToCommonButtonsPage() async {
+    await Get.toNamed(
+      Routes.commonButtons,
+    );
+  }
+
+  Future<void> goToFabPage() async {
+    await Get.toNamed(
+      Routes.fab,
+    );
+  }
+
+  Future<void> goToExtendedFabPage() async {
+    await Get.toNamed(
+      Routes.extendedFab,
+    );
+  }
+
+  Future<void> goToIconButtonPage() async {
+    await Get.toNamed(
+      Routes.iconButton,
+    );
+  }
+
+  Future<void> goToSegmentedButtonPage() async {
+    await Get.toNamed(
+      Routes.segmentedButton,
+    );
+  }
+}
