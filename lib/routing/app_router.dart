2833fdcf7d5f3edee2e4b1d2e4e3ce5016dd796b
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-07 13:54:32 -0300

    feat: adicionando a pasta de common_buttons

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -2,7 +2,7 @@ import 'package:flutterplayground/routing/routes.dart';
 import 'package:flutterplayground/ui/home/home_bindings.dart';
 import 'package:flutterplayground/ui/material/material_bindings.dart';
 import 'package:flutterplayground/ui/material/material_page.dart';
-import 'package:flutterplayground/ui/material/pages/actions/common_buttons_page.dart';
+import 'package:flutterplayground/ui/material/pages/actions/common_buttons/common_buttons_page.dart';
 import 'package:flutterplayground/ui/material/pages/actions/extended_fab_page.dart';
 import 'package:flutterplayground/ui/material/pages/actions/fab_page.dart';
 import 'package:flutterplayground/ui/material/pages/actions/icon_button_page.dart';
