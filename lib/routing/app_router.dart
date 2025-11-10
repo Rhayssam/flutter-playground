a28b7f96ae912ee00618105adbc2312ca6b12804
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-10 08:41:43 -0300

    rename: movendo o LinearProgressIndicatorPage para a pasta linear_progress_indicator e alterando a rota no AppRouter

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -9,7 +9,7 @@ import 'package:flutterplayground/ui/material/pages/actions/icon_button/icon_but
 import 'package:flutterplayground/ui/material/pages/actions/segmented_button/segmented_button_bindings.dart';
 import 'package:flutterplayground/ui/material/pages/actions/segmented_button/segmented_button_page.dart';
 import 'package:flutterplayground/ui/material/pages/communication/badge/badge_page.dart';
-import 'package:flutterplayground/ui/material/pages/communication/linear_progress_indicator_page.dart';
+import 'package:flutterplayground/ui/material/pages/communication/linear_progress_indicator/linear_progress_indicator_page.dart';
 import 'package:flutterplayground/ui/material/pages/communication/snackbar_page.dart';
 import 'package:flutterplayground/ui/splash/splash_bindings.dart';
 
