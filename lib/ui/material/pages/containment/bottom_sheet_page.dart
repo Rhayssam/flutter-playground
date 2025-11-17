01d8a0a41b7229df4147fd782efe939ed0eae026
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-17 17:45:27 -0300

    feat: adicionando a BottomSheetPage

---
+++ lib/ui/material/pages/containment/bottom_sheet_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetPage extends StatelessWidget {
+  const BottomSheetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
