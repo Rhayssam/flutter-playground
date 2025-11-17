ca0ed51aff570273a929f84175d7a645b6a5ed57
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-17 17:45:05 -0300

    feat: adicionando a AlertDialogPage

---
+++ lib/ui/material/pages/containment/alert_dialog_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlertDialogPage extends StatelessWidget {
+  const AlertDialogPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
