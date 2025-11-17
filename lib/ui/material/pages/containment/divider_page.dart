8aa68545e78f7cefc55cb81bf92a28045102bc24
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-17 17:45:53 -0300

    feat: adicionando a DividerPage

---
+++ lib/ui/material/pages/containment/divider_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DividerPage extends StatelessWidget {
+  const DividerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
