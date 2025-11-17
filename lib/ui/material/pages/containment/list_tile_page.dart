463397d7ee759bf28c71865dacd39788fd7cf25b
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-17 17:46:13 -0300

    feat: adicionando a ListTilePage

---
+++ lib/ui/material/pages/containment/list_tile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListTilePage extends StatelessWidget {
+  const ListTilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
