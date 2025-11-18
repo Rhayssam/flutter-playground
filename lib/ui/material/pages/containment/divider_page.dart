0f1112b8d5fd05981b404d7584dad8237c68a63e
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-18 17:45:34 -0300

    feat: adicionando os atributos do Divider na DividerPage

--- lib/ui/material/pages/containment/divider_page.dart
+++ lib/ui/material/pages/containment/divider_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/app_bar_back_button.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
 
 class DividerPage extends StatelessWidget {
   const DividerPage({super.key});
@@ -6,10 +8,49 @@ class DividerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: CustomAppBar.primaryContainer(
+        title: Text('Card'),
+        leading: AppBarBackButton(),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            spacing: 10,
+            children: [
+              Text('Esse aqui é bem simples... é literalmente um divisor.'),
+              Text('Esse cara aqui.'),
+              Icon(Icons.arrow_downward),
+              Divider(),
+              Text('É possível alterar a cor dele'),
+              Divider(color: Theme.of(context).colorScheme.primary),
+              Text('Também é possível alterar a espessura dele'),
+              Divider(thickness: 3, color: Theme.of(context).colorScheme.primary),
+              Text(
+                'Também é possível alterar identação (do início e do final da linha de divisão)',
+              ),
+              Divider(
+                thickness: 3,
+                indent: 20,
+                endIndent: 70,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              Text(
+                'É possível também alterar as bordas (deixar as bordas arredondadas)',
+              ),
+              Divider(
+                thickness: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              Divider(
+                thickness: 20,
+                color: Theme.of(context).colorScheme.primary,
+                radius: BorderRadiusGeometry.all(Radius.circular(10)),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
