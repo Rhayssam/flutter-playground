d46409b310084e0e0034d7dee0fc791374b59aaf
rhayssam <rhayssam@crmsolucoes.com.br>
2025-11-18 16:23:12 -0300

    feat: adicionando os exemplos de bottomSheet na BottomSheetPage

--- lib/ui/material/pages/containment/bottom_sheet_page.dart
+++ lib/ui/material/pages/containment/bottom_sheet_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/app_bar_back_button.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
+import 'package:get/get.dart';
 
 class BottomSheetPage extends StatelessWidget {
   const BottomSheetPage({super.key});
@@ -6,10 +9,69 @@ class BottomSheetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: CustomAppBar.primaryContainer(
+        title: Text('BottomSheet'),
+        leading: AppBarBackButton(),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            spacing: 20,
+            children: [
+              ElevatedButton(
+                child: Text('Bottom Sheet com Container'),
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        width: double.infinity,
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          spacing: 20,
+                          children: [
+                            const SizedBox(height: 50),
+                            Text('Conteúdo do BottomSheet'),
+                            ElevatedButton(onPressed: () => Get.back(), child: Text('Fechar')),
+                            const SizedBox(height: 50),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: Text('Bottom Sheet com SizedBox'),
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (context) {
+                      return SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          spacing: 20,
+                          children: [
+                            const SizedBox(height: 50),
+                            Text('Conteúdo do BottomSheet'),
+                            ElevatedButton(onPressed: () => Get.back(), child: Text('Fechar')),
+                            const SizedBox(height: 50),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
