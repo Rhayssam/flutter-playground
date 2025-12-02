6d9ed6de1c613f4ea60774df29bc475a351549e5
rhayssam <rhayssam@crmsolucoes.com.br>
2025-12-02 14:07:39 -0300

    feat: add appBar examples on AppBarPage

--- lib/ui/material/pages/navigation/app_bar/app_bar_page.dart
+++ lib/ui/material/pages/navigation/app_bar/app_bar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
 
 class AppBarPage extends StatelessWidget {
   const AppBarPage({super.key});
@@ -7,9 +8,23 @@ class AppBarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Essa é a AppBar padrão'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          AppBar(
+            leading: Icon(Icons.cookie),
+            backgroundColor: Colors.amber,
+            title: Text('Ela sempre fica no topo'),
+          ),
+          CustomAppBar.primary(
+            title: Text('Posso ter ações --->'),
+          ),
+          CustomAppBar.primaryContainer(
+            title: Text('Posso ter várias cores'),
+          ),
+        ],
+      ),
     );
   }
 }
