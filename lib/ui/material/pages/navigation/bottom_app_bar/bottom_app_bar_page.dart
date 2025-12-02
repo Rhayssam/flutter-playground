4252d3153317949ca1fae06dc0f537e2c2557355
rhayssam <rhayssam@crmsolucoes.com.br>
2025-12-02 14:30:50 -0300

    feat: add BottomAppBarPage

---
+++ lib/ui/material/pages/navigation/bottom_app_bar/bottom_app_bar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomAppBarPage extends StatelessWidget {
+  const BottomAppBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomAppBarPage'),
+      ),
+      body: Container(),
+    );
+  }
+}
