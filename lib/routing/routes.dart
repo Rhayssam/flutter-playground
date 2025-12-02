f654c46fd7cffd207223e33d0ed41a16c023ff0a
rhayssam <rhayssam@crmsolucoes.com.br>
2025-12-02 13:41:59 -0300

    feat: add appBar route on Routes

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -19,6 +19,8 @@ final class Routes {
   static const String card = '/material/containment/card';
   static const String divider = '/material/containment/divider';
   static const String listTile = '/material/containment/listTile';
+  // 1.4 - Navigation
+  static const String appBar = '/material/navigation/appBar';
 
   static const String notFound =
       '/'; //! O Getx sempre redireciona para a rota raiz se a rota não for encontrada
