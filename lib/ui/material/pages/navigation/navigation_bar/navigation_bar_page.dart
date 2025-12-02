62569f344a5f76740624d54bb651e1a1794bbb5a
rhayssam <rhayssam@crmsolucoes.com.br>
2025-12-02 16:41:47 -0300

    feat: add NavigationBar on NavigationBarPage

--- lib/ui/material/pages/navigation/navigation_bar/navigation_bar_page.dart
+++ lib/ui/material/pages/navigation/navigation_bar/navigation_bar_page.dart
@@ -9,7 +9,23 @@ class NavigationBarPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('NavigationBar'),
       ),
-      body: Container(),
+      body: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Messages',
+          ),
+        ],
+      ),
     );
   }
 }
