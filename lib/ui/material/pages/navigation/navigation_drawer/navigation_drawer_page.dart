2dff4496e40ab46892efe58f3acd378b02e6c3bb
rhayssam <rhayssam@crmsolucoes.com.br>
2025-12-03 09:07:06 -0300

    feat: add content to NavigationDrawerPage

--- lib/ui/material/pages/navigation/navigation_drawer/navigation_drawer_page.dart
+++ lib/ui/material/pages/navigation/navigation_drawer/navigation_drawer_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/app_bar_back_button.dart';
+import 'package:flutterplayground/ui/core/widgets/app_bar/custom_app_bar.dart';
 
 class NavigationDrawerPage extends StatelessWidget {
   const NavigationDrawerPage({super.key});
@@ -6,10 +8,58 @@ class NavigationDrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: CustomAppBar.primaryContainer(
         title: const Text('NavigationDrawer'),
+        leading: AppBarBackButton(),
       ),
-      body: Container(),
+
+      drawer: _buildDrawer(),
+
+      body: Builder(
+        builder: (context) {
+          return Center(
+            child: ElevatedButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              child: const Text('Abrir NavigationDrawer'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildDrawer() {
+    return NavigationDrawer(
+      children: [
+        SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const DrawerHeader(
+                child: Text(
+                  'Menu',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Configurações'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Sair'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
